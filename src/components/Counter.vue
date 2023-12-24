42dcfca05da03416a446e2e81881435e6ed84777
Francisco Rangel <francorangelcoronado@gmail.com>
2023-12-23 23:02:45 -0600

    modificando el state por medio de las mutations

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -2,15 +2,34 @@
     <h1>Counter in Vuex</h1>
     <h2>Direct access: {{ $store.state.count }}</h2>
     <h2>Computed: {{ countComputed }}</h2>
+
+    <button @click="increment">+1</button>
+    <button @click="incrementBy">+5</button>
+    <button>Random</button>
+
+    <h2>mapState: {{ count }}</h2>
+    <h2>lastMutation: {{ lastMutation }}</h2>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
     name: 'Counter',
     computed: {
         countComputed() {
             return this.$store.state.count
-        }
+        },
+        ...mapState(['count', 'lastMutation'])
+    },
+    methods: {
+        increment() {
+            // Al commit le pasamos el nombre de la mutasion
+            this.$store.commit('increment')
+        },
+        incrementBy() {
+            this.$store.commit('incrementBy', 5)
+        },
     }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,20 @@ import { createStore } from 'vuex'
 export default createStore({
     state() {
         return {
-            count: 0
+            count: 0,
+            lastMutation: 'none'
         }
-    }
+    },
+    // Las mutations son sincronas y cambian el state
+    mutations: {
+        // Pasamos la referencia al state como parametro
+        increment(state) {
+            state.count++
+            state.lastMutation = 'Increment'
+        },
+        incrementBy(state, value) {
+            state.count += value
+            state.lastMutation = 'IncrementBy'
+        }
+    },
 })
