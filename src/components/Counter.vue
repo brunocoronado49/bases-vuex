cedf95f4e67ff6401935441f7711eb144d1422d4
Francisco Rangel <francorangelcoronado@gmail.com>
2023-12-23 18:40:25 -0600

    creando proyecto con bases de vuex

---
+++ src/components/Counter.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Counter in Vuex</h1>
+    <h2>Direct access: {{ $store.state.count }}</h2>
+    <h2>Computed: {{ countComputed }}</h2>
+</template>
+
+<script>
+export default {
+    name: 'Counter',
+    computed: {
+        countComputed() {
+            return this.$store.state.count
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
